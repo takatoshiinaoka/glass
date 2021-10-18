8558a969fa6bd2eb8099110c11e3f4bf514a9cef
takatoshiinaoka <heavenhayao2316@gmail.com>
2021-10-18 10:24:41 +0900

    検索viewから検索modelにデータを送るまで

---
+++ glass/view/index.php
@@ -0,0 +1,18 @@
+<?php
+// var_dump($_POST);
+// exit();
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>index</title>
+</head>
+<body>
+    <a href="serch_view.php">検索</a>
+  </form>
+</body>
+</html>
---
+++ glass/view/serch_view.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>検索</title>
+</head>
+<body>
+  <form action="/glass/glass/model/serch_model.php" method="POST">
+    <fieldset>
+      <legend>検索キーワード入力画面</legend>
+      <a href="index.php">戻る</a>
+      <table>
+      <tr>
+      <td>keyword:<input type="text" size="20" style="width:160px;"  name="keyword"/></td>
+      <td><button>検索</button></td>
+      </tr>
+      </table>
+    </fieldset>
+  </form>
+</body>
+</html>
